fb0e5c5cbd248cc6f9959c9348be3673b1b91cce
wooseok <gowoo97@gmail.com>
2024-04-22 14:27:23 +0900

    init

---
+++ src/test/java/com/elice/gather/GatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
